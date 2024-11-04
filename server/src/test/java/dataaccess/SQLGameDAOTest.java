974b7c57cc57d7da3ec31c87043f54018e4aabec
Brendan Harvey <brendanharvey123@gmail.com>
2024-11-03 22:37:43 -0700

    Finish all tests for sql dao classes

--- server/src/test/java/dataaccess/SQLGameDAOTest.java
+++ server/src/test/java/dataaccess/SQLGameDAOTest.java
@@ -1,6 +1,8 @@
 package dataaccess;
 
+import chess.ChessGame;
 import dataaccess.model.CreateGameReq;
+import dataaccess.model.JoinGameReq;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -33,20 +35,31 @@ class SQLGameDAOTest {
 
     @Test
     void makeGoodGame() {
+        Assertions.assertDoesNotThrow(() -> gdao.makeGame(new CreateGameReq("bgame")));
+    }
+
+    @Test
+    void makeBadGame() {
         String name = null;
         Assertions.assertThrows(DataAccessException.class, () -> gdao.makeGame(new CreateGameReq(name)));
     }
 
     @Test
-    void makeBadGame() {
-
+    void joinGoodGame() throws DataAccessException {
+        gdao.makeGame(new CreateGameReq("game1"));
+        Assertions.assertDoesNotThrow(() -> gdao.joinGame(new JoinGameReq(ChessGame.TeamColor.WHITE,1),"user"));
     }
 
     @Test
-    void joinGame() {
+    void joinTakenGame() throws DataAccessException {
+        gdao.makeGame(new CreateGameReq("game1"));
+        gdao.joinGame(new JoinGameReq(ChessGame.TeamColor.WHITE,1),"user1");
+        Assertions.assertThrows(DataAccessException.class, () -> gdao.joinGame(new JoinGameReq(ChessGame.TeamColor.WHITE,1),"user2"));
     }
 
     @Test
-    void deleteAllGames() {
+    void deleteAllGames() throws DataAccessException {
+        gdao.makeGame(new CreateGameReq("game1"));
+        Assertions.assertDoesNotThrow(() -> gdao.deleteAllGames());
     }
 }
